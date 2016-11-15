a1d9eece3ced49e97412aeaaa1d7e7a824f8be04
dholmstr <daniel.holmstrom@metropolia.fi>
2016-11-15 15:37:09 +0200

    Add files via upload
    Forgot some files on last upload.

--- autoCompleteTextBox.js
+++ autoCompleteTextBox.js
@@ -3,7 +3,7 @@ function getAddress(inputAddress){
 	$(inputAddress).autocomplete({
             source: function (request, response) {
                 $.ajax({
-                    url: "db/test",
+                    url: "test",
                     dataType: "json",
                     response: ($.map(data, function(v,i){
                         return {
